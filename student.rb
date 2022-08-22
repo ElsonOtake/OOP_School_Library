078f7f134c96747404d0c8b0a7d30150b9ae1098
Elson Otake <elsonotake@yahoo.com.br>
2022-08-22 17:25:47 -0300

    Update default value to string

--- student.rb
+++ student.rb
@@ -1,8 +1,7 @@
-require "./person.rb"
+require './person'
 
 class Student < Person
-  
-  def initialize(classroom, age, name = "Unknown", parent_permission = true)
+  def initialize(classroom, age, name = 'Unknown', parent_permission = 'true')
     super(age, name, parent_permission)
     @classroom = classroom
   end
