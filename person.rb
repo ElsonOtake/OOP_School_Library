ab68aea474391f2bfa983ce00866a4320dce6060
Elson Otake <elsonotake@yahoo.com.br>
2022-08-22 17:25:26 -0300

    Update method to private and default value to string

--- person.rb
+++ person.rb
@@ -2,18 +2,20 @@ class Person
   attr_reader :id
   attr_accessor :name, :age
 
-  def initialize(age, name = "Unknown", parent_permission = true)
+  def initialize(age, name = 'Unknown', parent_permission = 'true')
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def is_of_age?
-    @age.to_i >= 18 ? true : false
+  def can_use_services?
+    of_age? || @parent_permission
   end
 
-  def can_use_services?
-    self.is_of_age? || @parent_permission ? true : false
+  private
+
+  def of_age?
+    @age.to_i >= 18
   end
 end
