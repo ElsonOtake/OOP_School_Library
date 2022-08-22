84d3e925150d577b962f94b4bbcc02f8128a5fc2
Elson Otake <elsonotake@yahoo.com.br>
2022-08-22 17:26:05 -0300

    Update default value to string

--- teacher.rb
+++ teacher.rb
@@ -1,8 +1,7 @@
-require "./person.rb"
+require './person'
 
 class Teacher < Person
-
-  def initialize(specialization, age, name = "Unknown", parent_permission = true)
+  def initialize(specialization, age, name = 'Unknown', parent_permission = 'true')
     super(age, name, parent_permission)
     @specialization = specialization
   end
