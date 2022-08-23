eca992f300c9bfc351b5dd8ef230dce20938a890
Elson Otake <elsonotake@yahoo.com.br>
2022-08-23 14:45:35 -0300

    Fix linter error

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -3,6 +3,7 @@ require './decorator'
 class TrimmerDecorator < Decorator
   def correct_name
     return @nameable.correct_name.strip unless @nameable.correct_name.strip.length > 10
+
     @nameable.correct_name.strip.slice(0..9)
   end
 end
