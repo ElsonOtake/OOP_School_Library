a5bab829456c4ef3e3538a33c1f5e24378ae40f2
Elson Otake <elsonotake@yahoo.com.br>
2022-08-23 14:37:05 -0300

    Update correct_name method

--- trimmer_decorator.rb
+++ trimmer_decorator.rb
@@ -2,6 +2,7 @@ require './decorator'
 
 class TrimmerDecorator < Decorator
   def correct_name
-    "TrimmerDecorator(#{@nameable.correct_name})"
+    return @nameable.correct_name.strip unless @nameable.correct_name.strip.length > 10
+    @nameable.correct_name.strip.slice(0..9)
   end
 end
