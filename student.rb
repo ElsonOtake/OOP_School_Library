6401c6911c0158e33d6f04a68c3a6ae3f1939bc9
Elson Otake <elsonotake@yahoo.com.br>
2022-08-23 23:35:54 -0300

    Update add_classroom method

--- student.rb
+++ student.rb
@@ -2,13 +2,15 @@ require './person'
 
 class Student < Person
   attr_accessor :classroom
+
   def initialize(classroom, age, name = 'Unknown', parent_permission = 'true')
     super(age, name, parent_permission)
     @classroom = classroom
   end
-  def classroom=(classroom)
+
+  def add_classroom(classroom)
     @classroom = classroom
-    classroom.students.push(self) unless classroom.students.include?(self)
+    classroom.students << self unless classroom.students.include?(self)
   end
 
   def play_hooky
