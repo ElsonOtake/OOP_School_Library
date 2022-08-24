21b1aa7b656827d94bf0c2cb03ecc969f9a37bbc
Elson Otake <elsonotake@yahoo.com.br>
2022-08-23 23:36:59 -0300

    Update initialize method

--- rental.rb
+++ rental.rb
@@ -1,7 +1,11 @@
 class Rental
-  attr_accessor :date
+  attr_reader :person, :book, :date
 
-  def initialize(date)
+  def initialize(person, book, date)
+    @person = person
+    @book = book
     @date = date
+    person.rentals << self unless person.rentals.include?(self)
+    book.rentals << self unless book.rentals.include?(self)
   end
 end
