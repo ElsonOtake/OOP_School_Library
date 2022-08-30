ab883f6ff8a12558410f3503cf88f6fa805a5a2a
darikmohammed <r.m.darik@gmail.com>
2022-08-30 23:08:39 +0300

    Add writer for the people

--- json.rb
+++ json.rb
@@ -1,10 +1,17 @@
 require './app'
+require 'json'
 
 module Json_IO
   def writer
+    File.write("Data/books.json", "", mode: "w")
+    File.write("Data/peoples.json", "", mode: "w")
     @app.list_books.each do |book|
       json_book = JSON.generate(book)
-      File.write("books.json", "#{json_book}\n", mode: "a")
+      File.write("Data/books.json", "#{json_book}\n", mode: "a")
+    end
+    @app.list_people.each do |people|
+      json_peoples = JSON.generate(people)
+      File.write("Data/peoples.json", "#{json_peoples}\n", mode: "a")
     end
   end
 end
--- student.rb
+++ student.rb
@@ -6,6 +6,7 @@ class Student < Person
   def initialize(classroom, age, name = 'Unknown', parent_permission = 'true')
     super(age, name, parent_permission)
     @classroom = classroom
+    @parent_permission = parent_permission
   end
 
   def add_classroom(classroom)
@@ -15,5 +16,17 @@ class Student < Person
 
   def play_hooky
     "¯\(ツ)/¯"
+  end
+
+  # Serialize Foo object with its class name and arguments
+  def to_json(*args)
+    {
+      JSON.create_id  => self.class.name,
+      'a'             => [ @classroom, age, name, @parent_permission ]
+    }.to_json(*args)
+  end
+  # Deserialize JSON string by constructing new Foo object with arguments.
+  def self.json_create(object)
+    new(*object['a'])
   end
 end
