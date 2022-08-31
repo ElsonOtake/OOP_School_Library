c061b4bc5dd05c91d202e97d08161d3fda9941e7
Elson Otake <elsonotake@yahoo.com.br>
2022-08-31 19:30:49 -0300

    Update tests for Classroom

--- spec/classroom_spec.rb
+++ spec/classroom_spec.rb
@@ -6,15 +6,31 @@ describe Classroom do
   end
 
   describe "#new" do
-    context "With another parameters" do
-      it "Has no label" do
-        classroom = Classroom.new("Chemistry")
-        expect(classroom.label).not_to eq "Math"
-      end
-    end
     context "With parameters defined on before each" do
+      it "Has instance of Classroom" do
+        expect(@classroom).to be_instance_of Classroom
+      end
+
       it "Has Math as label" do
         expect(@classroom.label).to eq "Math"
+      end
+
+      it "Has empty students enroled" do
+        expect(@classroom.students.length).to be 0
+      end
+
+      it "Has how to add new student to the class" do
+        john = Student.new(@classroom, 99, "John")
+        @classroom.add_student(john)
+        expect(@classroom.students.length).to be 1
+        expect(@classroom.students[0].name).to eq "John"
+        expect(@classroom.students[0]).to be_instance_of Student
+      end
+    end
+    context "With new parameters" do
+      it "Has new label" do
+        classroom = Classroom.new("Chemistry")
+        expect(classroom.label).not_to eq "Math"
       end
     end
   end
