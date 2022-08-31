500a8ec6735ab67bf1bfac2020dba78ff9d7435f
darikmohammed <r.m.darik@gmail.com>
2022-09-01 01:43:41 +0300

    Add test for Rental class

--- spec/rental_spec.rb
+++ spec/rental_spec.rb
@@ -1,22 +1,35 @@
 require 'spec_helper'
 
 describe Rental do
-  it "The creation of a new rental needs a Book and a Person" do
-    # book = double('book')
-    # person = double('person')
-
-    # allow(book).to receive(:title) { "Lord of the rings" }
-    # allow(book).to receive(:author) { "Tolkien" }
-    # allow(person).to receive(:age) { 24 }
-    book = Book.new("Lord of the rings", "Tolkien")
-    person = Person.new(24)
-
-    rental = Rental.new(person, book, '2022/08/31')
-    expect(rental).to be_instance_of Rental
-    expect(rental.person.age).to eq 24
-    expect(rental.book.author).to eq "Tolkien"
-    expect(rental.book.author).not_to eq "Lord of the rings"
-    expect(rental.person.rentals.length).to be > 0
-    expect(rental.book.rentals.length).to be > 0
+  before :each do
+    @book = Book.new("Lord of the rings", "Tolkien")
+    @person = Person.new(24)
+    @rental = Rental.new(@person, @book, '2022/08/31')
   end
+  describe "#new" do
+    it "rental Should be class of Rental" do
+      expect(@rental).to be_instance_of Rental
+    end
+
+    it "the rental book author and name should be Tolkien and Lord of the ring" do
+      expect(@rental.book.author).to eq "Tolkien"
+      expect(@rental.book.title).to eq "Lord of the rings"
+    end
+
+    it "person should be an instance of Person" do
+      expect(@rental.person).to be_instance_of Person
+      expect(@rental.person.name).to eq "Unknown"
+      expect(@rental.person.age).to be 24
+    end
+
+    it "book should be an instance of Book" do
+      expect(@rental.book).to be_instance_of Book
+    end
+
+    it "should have a rental" do
+      expect(@rental.person.rentals.length).to be > 0
+      expect(@rental.book.rentals.length).to be > 0
+    end
+  end
+
 end
