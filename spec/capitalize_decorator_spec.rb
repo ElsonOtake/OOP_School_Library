ae24178cab3dc4c77df6b7a1416749247e0191e0
Elson Otake <elsonotake@yahoo.com.br>
2022-08-31 19:54:32 -0300

    Add tests for CapitalizeDecorator

--- spec/capitalize_decorator_spec.rb
+++ spec/capitalize_decorator_spec.rb
@@ -1,20 +1,9 @@
 require 'spec_helper'
 
-# describe CapitalizeDecorator do
-#   it "#new" do
-#     capitalize = CapitalizeDecorator.new("mario")
-#     expect(capitalize).to be_instance_of CapitalizeDecorator
-#     expect(capitalize.nameable).to eq "mario"
-#     expect(capitalize.correct_name).to eq "Mario"
-#   end
-# end
-
 describe CapitalizeDecorator do
   it "test correct name method" do
     person = Person.new(22, 'maximilianus')
     capitalize = CapitalizeDecorator.new(person)
     expect(capitalize.correct_name).to eq "Maximilianus"
-    # capitalizedTrimmedPerson = TrimmerDecorator.new(capitalizedPerson)
-    # capitalizedTrimmedPerson.correct_name
   end
 end
