20e1f6d993977b9a54bbc191fbe63a65b086d8b8
Elson Otake <elsonotake@yahoo.com.br>
2022-08-31 20:26:01 -0300

    Add tests for Decorator

--- spec/decorator_spec.rb
+++ spec/decorator_spec.rb
@@ -1,8 +1,20 @@
 require 'spec_helper'
 
 describe Decorator do
-  it "New decorator" do
-    decorator = Decorator.new("mario")
-    expect(decorator.nameable).to eq "mario"
+  before :each do
+    @maximilianus = Person.new(31, "maximilianus")
+    @decorator = Decorator.new(@maximilianus)
+  end
+
+  it "Has instance of Decorator" do
+    expect(@decorator).to be_instance_of Decorator
+  end
+
+  it "Has name as maximilianus" do
+    expect(@decorator.nameable.name).to eq "maximilianus"
+  end
+
+  it "Has correct name as the same name" do
+    expect(@decorator.correct_name).to eq "maximilianus"
   end
 end
